5a453b0ee04da185bd2950f557065ef477cfba02
k-christy <cmkoesters@unomaha.edu>
2018-02-27 22:19:52 -0600

    Inital Mobile App Template

---
+++ MobileApp/PotteryPortal/app/src/main/java/com/pottery/christy/potteryportal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pottery.christy.potteryportal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
