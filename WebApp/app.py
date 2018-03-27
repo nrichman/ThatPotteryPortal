9e1d5c1f27289295f1b14bc895261466a658f595
NICK <nrichman962@gmail.com>
2018-03-26 20:30:05 -0500

    Made app.py RESTful

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 import MySQLdb
 
 app = Flask(__name__)
@@ -10,16 +11,32 @@ db = MySQLdb.connect(host="pottery-db.c30pytquwht8.us-east-2.rds.amazonaws.com",
                      passwd="jenkinspottery",
                      db="pottery_data")
 
-@app.route('/')
+
+def get_posts():
+    cur = db.cursor()
+    cur.execute("SELECT * FROM pet")
+    data = []
+    for row in cur.fetchall():
+        user_name, pet_name, date_time, image_url = row
+        data.append({
+            'user': user_name,
+            'pet': pet_name,
+            'time': str(date_time),
+            'image': image_url
+        })
+    return data
+
+@app.route('/test')
 def users():
     cur = db.cursor()
     cur.execute("SELECT * FROM pet")
     data = []
     for row in cur.fetchall():
         data.append(row)
-    return render_template('home.html', data=data)
+    return jsonify({'orders': get_posts()})
+    #return render_template('home.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-~
+    #app.run(debug=True)
+    get_posts()
