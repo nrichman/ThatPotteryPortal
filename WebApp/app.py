3e12a7656ed883ee393f8917c41d204052d52ae1
NICK <nrichman962@gmail.com>
2018-04-03 20:20:33 -0500

    Added tensorflow item names to insert order

--- WebApp/app.py
+++ WebApp/app.py
@@ -42,11 +42,16 @@ def insert_order():
     order_phone = data['phone']
     order_email = data['email']
     order_notes = data['notes']
-    order_items = data['num_items']    
-
+    order_num_items = data['num_items']    
+    order_items = data['order_items']
     add_word = "INSERT INTO order_data VALUES (%s, %s, %s, %s, %s, %s, %s, %s, NOW())"
     cur.execute(add_word, [order_number, order_name, order_phone, order_email, order_notes, order_items, 'FIRE', 'None'])
     db.commit()
+
+    for item in order_items.split(','):
+        add_item = "INSERT INTO order_items VALUES (%s, %s)"
+        cur.execute(add_item, [order_number, item])
+        db.commit()
     return 'Nice'
 
 @app.route('/insert_items', methods=['POST'])
@@ -93,7 +98,6 @@ def get_orders():
 
 @app.route('/get_order_num', methods=['GET'])
 def get_order_num():
-    print 'yo'
     cur = db.cursor()
     cur.execute("SELECT * FROM order_num")
     db.commit()
